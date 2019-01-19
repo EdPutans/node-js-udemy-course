1940626401f58a5cfcf89cfbb17351ab265aad5d
Edgar <edputans@gmail.com>
2019-01-19 21:10:58 +0000

    3-11 done

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const fs = require('fs')
 const os = require('os')
 const notes = require('./notes.js')
 
-fs.appendFileSync('yo.txt', `Henlo, I logged in as ${os.userInfo.username}`)
+// fs.appendFileSync('yo.txt', `Henlo, I logged in as ${os.userInfo.username}`)
+
+let result = notes.add(1, 6)
+
+console.log(result)
