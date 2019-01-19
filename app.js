8ea8c520ac11ec0d849d62a94b9172ddb490d00d
Edgar <edputans@gmail.com>
2019-01-19 21:21:36 +0000

    done 3.11

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ console.log('starting app.js...')
 const fs = require('fs')
 const os = require('os')
 const notes = require('./notes.js')
+const _ = require('lodash')
 
+// 3.9
 // fs.appendFileSync('yo.txt', `Henlo, I logged in as ${os.userInfo.username}`)
 
-let result = notes.add(1, 6)
+// 3.10
+// let result = notes.add(1, 6)
+// console.log(result)
 
-console.log(result)
+// 3.12
+console.log(_.isString("YEEET"))
+console.log(_.isString(true))
+console.log(`Uniques: ${_.uniq([1,2,2,3,4])}`)
